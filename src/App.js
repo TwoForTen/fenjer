cb1677fce350fd593e622a0ff5dc51ddaa9489b3
Noel Rostohar <noel.rostohar@gmail.com>
2020-05-02 16:55:24 +0200

    Added dependenices, implemented Redux

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import './App.css';
 
 function App() {
-  return <div>Fenjer.hr</div>;
+  return (
+    <>
+      <h1>Fenjer.hr</h1>
+    </>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './styles/_main.scss';
+
+import reducers from './reducers/index';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
