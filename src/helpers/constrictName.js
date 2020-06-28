685d2a80a376c116826e0df977b2742e8cc8a5b5
Noel Rostohar <noel.rostohar@gmail.com>
2020-06-28 15:59:46 +0200

    constrictName Helper Function Bug Fix

--- src/helpers/constrictName.js
+++ src/helpers/constrictName.js
@@ -2,7 +2,7 @@ const constrictName = (name) => {
   const MAX_LENGTH = 40;
 
   if (name.split('').length > MAX_LENGTH) {
-    return name.split('').slice(0, MAX_LENGTH).join(' ') + '...';
+    return name.split('').slice(0, MAX_LENGTH).join('') + '...';
   }
 
   return name;
