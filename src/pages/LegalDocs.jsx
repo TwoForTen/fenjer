6cd075cbcbcd7a798231269d4ce3262eeabbdd23
Noel Rostohar <noel.rostohar@gmail.com>
2020-06-30 15:44:24 +0200

    Legal Docs Update

--- src/pages/LegalDocs.jsx
+++ src/pages/LegalDocs.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
 
+import CircularProgress from '@material-ui/core/CircularProgress';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+
+import useDataFetch from '../hooks/useDataFetch';
 
 import PageBreadcrumbs from '../components/PageBreadcrumbs';
 
@@ -19,42 +22,59 @@ const useStyles = makeStyles((theme) => ({
 const LegalDocs = () => {
   const classes = useStyles();
 
+  const data = useDataFetch({
+    url: '/master-data',
+    method: 'GET',
+  });
+
+  console.log(data);
+
   return (
     <>
       <Helmet titleTemplate="%s | Fenjer.hr">
         <title>Matični Podaci</title>
       </Helmet>
       <PageBreadcrumbs titles={['Matični podaci']} />
-      <div className={classes.paragraphContainer}>
-        <Typography color="textPrimary" variant="h4" element="h2">
-          DEKORACIJE MAVRIN d.o.o.
-        </Typography>
-        <Typography color="textPrimary" variant="body1" paragraph>
-          Veleprodaja i maloprodaja cvijeća
-        </Typography>
-        <Typography color="textPrimary" variant="body1" paragraph>
-          Osnovano:
-          <br />
-          Adresa središnjice:
-          <br />
-          Sud upisa:
-          <br />
-          OIB:
-          <br />
-          <br />
-          Temeljni kapital je
-          <br />
-          Uprava:
-        </Typography>
-        <Typography color="textPrimary" variant="h6" element="h2">
-          Besplatna dostava:
-        </Typography>
-        <Typography color="textPrimary" variant="body1" paragraph>
-          Žiro račun:
-          <br />
-          IBAN:
-        </Typography>
-      </div>
+      {data ? (
+        <div className={classes.paragraphContainer}>
+          <Typography color="textPrimary" variant="h4" element="h2">
+            DEKORACIJE MAVRIN d.o.o.
+          </Typography>
+          <Typography color="textSecondary" variant="body1" paragraph>
+            Veleprodaja i maloprodaja cvijeća
+          </Typography>
+          <Typography color="textSecondary" variant="body1" paragraph>
+            Osnovano: {data.founded}
+            <br />
+            Adresa središnjice: {data.headquarters_address}
+            <br />
+            Sud upisa: {data.registered_in_court}
+            <br />
+            OIB: {data.oib}
+            <br />
+            <br />
+            Temeljni kapital je{' '}
+            {new Intl.NumberFormat('hr-HR', {
+              style: 'currency',
+              currency: 'HRK',
+            }).format(data.starting_capital)}
+            <br />
+            Uprava: {data.corporate}
+          </Typography>
+          <Typography color="textPrimary" variant="h6" element="h2">
+            Besplatna dostava:
+          </Typography>
+          <Typography color="textSecondary" variant="body1" paragraph>
+            Žiro račun: {data.giro}
+            <br />
+            IBAN: {data.iban}
+          </Typography>
+        </div>
+      ) : (
+        <div style={{ textAlign: 'center' }} className="mt-4 mb-4">
+          <CircularProgress />
+        </div>
+      )}
     </>
   );
 };
