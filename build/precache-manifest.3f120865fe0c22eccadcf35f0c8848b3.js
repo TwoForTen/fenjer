7f1c9d0a0cb2f3394d2222b0111cd488d542abf5
Toni Krešić <tonikresic1997@gmail.com>
2020-07-13 12:53:37 +0200

    build version

---
+++ build/precache-manifest.3f120865fe0c22eccadcf35f0c8848b3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3834cfb80002a38cffe4670a2407f19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a565d1acd828f2ec1b34",
+    "url": "/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "f016e2f4254fa4bd360a",
+    "url": "/static/css/main.7611314b.chunk.css"
+  },
+  {
+    "revision": "a565d1acd828f2ec1b34",
+    "url": "/static/js/2.89fd889d.chunk.js"
+  },
+  {
+    "revision": "479f0a747c5a85c6dbe59df8d7f347b2",
+    "url": "/static/js/2.89fd889d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f016e2f4254fa4bd360a",
+    "url": "/static/js/main.0590ca89.chunk.js"
+  },
+  {
+    "revision": "4107193b05f44cd0d47b",
+    "url": "/static/js/runtime-main.fa06e83e.js"
+  },
+  {
+    "revision": "c3fb010b891b9efa411c58d1a7ad79b7",
+    "url": "/static/media/404_logo.c3fb010b.png"
+  },
+  {
+    "revision": "cf9a3d0fca54f4a3a59dae44a2c7a58f",
+    "url": "/static/media/delivery_icon.cf9a3d0f.svg"
+  },
+  {
+    "revision": "09a16a56d0136f215f802230f759f805",
+    "url": "/static/media/fenjer_logo.09a16a56.svg"
+  },
+  {
+    "revision": "ade8ae209289cc65e1bbbccf8aa52877",
+    "url": "/static/media/home_carousel1.ade8ae20.jpg"
+  },
+  {
+    "revision": "c44a73f95c24f8bec6dd40ccfe0c57de",
+    "url": "/static/media/home_carousel2.c44a73f9.jpg"
+  },
+  {
+    "revision": "df0e952997d0d5f0fbb2e95109684b28",
+    "url": "/static/media/home_carousel3.df0e9529.jpg"
+  },
+  {
+    "revision": "d1918d72d3c5c9187d35bd2392eaa92b",
+    "url": "/static/media/home_carousel4.d1918d72.png"
+  }
+]);
