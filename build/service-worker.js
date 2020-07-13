f77248fdb4e152ae7716fa6d35425798e1da54f3
Toni Krešić <tonikresic1997@gmail.com>
2020-07-13 11:41:29 +0200

    build version

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4477d2eaba2463f519ac5e052b31ebd9.js"
+  "/precache-manifest.a1bf66c8517c8baf52ae7cfb3d453081.js"
 );
 
 self.addEventListener('message', (event) => {
